ac1037dc05dae74699311768e0ebb39f4ddfb695
Bigsby <bigsby@live.com>
2017-06-05 01:41:54 +0100

    07. HTTP

--- tour-of-heroes/src/app/heroes.component.ts
+++ tour-of-heroes/src/app/heroes.component.ts
@@ -7,7 +7,7 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-heroes',
   templateUrl: "./heroes.component.html",
-  styleUrls:[
+  styleUrls: [
     "./heroes.component.css"
   ]
 })
@@ -28,4 +28,22 @@ export class HeroesComponent implements OnInit {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
 }
