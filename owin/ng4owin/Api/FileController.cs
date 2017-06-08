6f8a0562b7db75defa657b6bf60ecf55dd5e9735
Bruno Bernardo <bigsby@live.com>
2017-06-08 19:04:31 +0100

    Clean up

--- owin/ng4owin/Api/FileController.cs
+++ owin/ng4owin/Api/FileController.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Hosting;
+﻿using System.Web.Hosting;
 using System.Web.Http;
 using System.Net.Http;
 using System.IO;
@@ -12,7 +11,6 @@ namespace ng4owin.Api
         public async void Upload()
         {
             var folderPath = HostingEnvironment.MapPath("~/uploads");
-            //var request = HttpContext.Current.Request;
             var isMultiPart = Request.Content.IsMimeMultipartContent();
             if (isMultiPart)
             {
@@ -21,19 +19,7 @@ namespace ng4owin.Api
                     using (var fileStream = File.OpenWrite(Path.Combine(folderPath, content.Headers.ContentDisposition.FileName.Trim('"'))))
                         await content.CopyToAsync(fileStream);
             }
-            //if (isMultiPart) {
-            //var streamProvider = new MultipartFormDataStreamProvider(HostingEnvironment.MapPath("~/uploads"));
-            //await Request.Content.ReadAsMultipartAsync(streamProvider);
-            //}
-            //if (request.Files?.Count > 0) {
 
-            //    foreach (string file in request.Files)
-            //    {
-            //        var postFile = request.Files[file];
-            //        var path = HostingEnvironment.MapPath("~/uploads/" + postFile.FileName);
-            //        postFile.SaveAs(path);
-            //    }
-            //}
 
         }
     }
